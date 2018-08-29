91581be02bda7f4078b619f3bb67fe87b04f0b54
Wael Almestady <almestady@gmail.com>
2018-08-29 11:20:15 +0300

    Update config to use json file

--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const hbs = require('hbs');
+const path = require('path');
 
 const publicPath = path.join(__dirname,'../public');
-const port = process.env.PORT || 5000
-var app  = express(); 
+const port = process.env.PORT || 5000;
+var app  = express();
 
 app.use(express.static(publicPath));
 
-app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
-  res.render('index.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
-  });
-});
+// app.set('view engine', 'hbs');
+//
+// app.get('/', (req, res) => {
+//   res.render('index.hbs', {
+//     pageTitle: 'Home Page',
+//     welcomeMessage: 'Welcome to my website'
+//   });
+// });
 
 app.listen(5000, () => {
   console.log(`Server is up on port 5000`);
