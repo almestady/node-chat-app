dcbe54955950b33edcb350ff717e2996360efdf8
Wael Almestady <almestady@gmail.com>
2018-09-03 17:06:20 +0300

    Add moment time format to the app

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,19 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
+  var formatedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formatedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
+  var formatedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
+  var a = jQuery(`<a target="_blank">My current location</a>`);
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from}  ${formatedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
@@ -29,11 +30,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextBox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
     text: jQuery('[name=message]').val()
   }, function () {
-
+  messageTextBox.val();
   });
 });
 
@@ -42,13 +45,17 @@ locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
   }
+  locationButton.attr('disabled','disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
+
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
   });
 });
